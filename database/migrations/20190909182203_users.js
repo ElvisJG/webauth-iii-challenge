8f1404ff3d71838de770c938c73f980aeaa15811
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-09-09 18:26:41 -0400

    AuthDB, migrations, server.js, index.js, knexfile.js, and dependancies

---
+++ database/migrations/20190909182203_users.js
@@ -0,0 +1,17 @@
+// Creates users table with an auto-incrementing ID, a username (128 chars, required, unique)
+// and a password (128 chars, required)
+exports.up = function(knex) {
+  return knex.schema.createTable('users', users => {
+    users.increments();
+    users
+      .string('username', 128)
+      .notNullable()
+      .unique();
+    users.string('password', 128).notNullable();
+  });
+};
+
+// Deletes users table
+exports.down = function(knex) {
+  return knex.schema.dropTablesIfExists('users');
+};
